e1d6418d208eedfae47ab3c2c65a80746e2a1750
Rythwin23 <ouafi.rabah01@gmail.com>
2026-02-08 00:48:39 +0100

    train24bis

--- nanoGPTsample/train_with_metrics.py
+++ nanoGPTsample/train_with_metrics.py
@@ -34,16 +34,16 @@ wandb_run_name = 'gpt2'  # 'run' + str(time.time())
 dataset = './data/cosmopedia-100k-Unigram'
 
 gradient_accumulation_steps = 5  # used to simulate larger batch sizes
-batch_size = 32  # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 16  # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 512
 # model
-n_layer = 6  # 12
-n_head = 6  # 12
-n_embd = 384  # 768
+n_layer = 8  # 12
+n_head = 8  # 12
+n_embd = 512  # 768
 dropout = 0  # for pretraining 0 is good, for finetuning try 0.1+
 bias = False  # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
-learning_rate = 0.0006  # max learning rate
+learning_rate = 0.001  # max learning rate
 max_iters = 5000  # total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
@@ -53,7 +53,7 @@ grad_clip = 1.0  # clip gradients at this value, or disable if == 0.0
 decay_lr = True  # whether to decay the learning rate
 warmup_iters = 500  # how many steps to warm up for
 lr_decay_iters = 5000  # should be ~= max_iters per Chinchilla
-min_lr = 0.00006  # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+min_lr = 0.0001  # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
 backend = 'nccl'  # 'nccl', 'gloo', etc.
 # system
